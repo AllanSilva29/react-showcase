8c0af750e244efb1f01aea2c28d134f06e09bc85
allanjulio836 <allanjulio836@gmail.com>
2025-05-30 05:55:45 +0000

    [add] Adicionado o arquivo de conf do tailwind para build correto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{css,js,ts,jsx,tsx,vue}", // adapte ao seu projeto
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
